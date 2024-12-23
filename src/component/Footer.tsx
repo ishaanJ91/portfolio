fe1df9293043e5d3e1730f3c8cba47183ab883e9
ishaanJ91 <jaini@tcd.ie>
2024-12-23 13:58:11 +0400

    footer responsive

--- src/component/Footer.tsx
+++ src/component/Footer.tsx
@@ -4,7 +4,7 @@ export default function Footer() {
   return (
     <footer className="bg-black text-white py-6">
       <div className="mx-auto px-4 py-6 flex justify-center items-center">
-        <div className="text-sm justify-center items-center text-center">
+        <div className="sm:text-sm xs:text-xs justify-center items-center text-center">
           <p>Handcrafted by me using React, Typescript and Tailwind CSS.</p>
           <p>Copyright © 2024. All rights are reserved</p>
         </div>
